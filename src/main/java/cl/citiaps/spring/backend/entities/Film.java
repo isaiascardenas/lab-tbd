361a4110cf0df4d2822e3aff50b0ab4b23b63efd
Unknown <jorge.plaza@usach.cl>
2018-10-27 14:54:29 -0300

    Metodos GET listos
    /actors/1/films
    /films/1/actors
    funcionando

--- src/main/java/cl/citiaps/spring/backend/entities/Film.java
+++ src/main/java/cl/citiaps/spring/backend/entities/Film.java
@@ -1,6 +1,171 @@
 package cl.citiaps.spring.backend.entities;
 
-public class Film {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.List;
 
+
+@Entity
+@Table(name="film")
+@NamedQuery(name="Film.findAll", query="SELECT f FROM Film f")
+public class Film implements Serializable {
+    private static final long serialVersionUID = 2L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name="film_id", unique=true, nullable = false)
+    private int filmId;
+
+    @Column(name = "title", nullable = false, length = 45)
+    private String filmTitle;
+
+    @Column(name = "description", nullable = false)
+    private String filmDescription;
+
+    @Column(name = "release_year", nullable = false)
+    private int filmYear;
+
+    @Column(name = "language_id", nullable = false)
+    private int filmLanguageId;
+
+    @Column(name = "original_language_id")
+    private Long filmOriginalLanguajeId;
+
+    @Column(name = "rental_duration", nullable = false)
+    private int filmRentalDuration;
+
+    @Column(name = "rental_rate", nullable = false)
+    private double filmRentalRate;
+
+    @Column(name = "length", nullable = false)
+    private int filmLength;
+
+    @Column(name = "replacement_cost", nullable = false)
+    private double filmReplacementCost;
+
+    @Column(name = "rating", nullable = false)
+    private String filmRating;
+
+    @Column(name = "special_features", nullable = false)
+    private String filmFeatures;
+
+    @Column(name = "last_update", nullable = false)
+    private Timestamp lastUpdate;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "film_actor",
+            joinColumns = @JoinColumn(name = "actor_id", referencedColumnName = "film_id"),
+            inverseJoinColumns = @JoinColumn(name="film_id",
+                    referencedColumnName = "actor_id"))
+    private List<Actor> actors;
+
+    public Film() {
+
+    }
+
+    public int getFilmId() {
+        return filmId;
+    }
+
+    public void setFilmId(int filmId) {
+        this.filmId = filmId;
+    }
+
+    public String getFilmTitle() {
+        return filmTitle;
+    }
+
+    public void setFilmTitle(String filmTitle) {
+        this.filmTitle = filmTitle;
+    }
+
+    public String getFilmDescription() {
+        return filmDescription;
+    }
+
+    public void setFilmDescription(String filmDescription) {
+        this.filmDescription = filmDescription;
+    }
+
+    public int getFilmYear() {
+        return filmYear;
+    }
+
+    public void setFilmYear(int filmYear) {
+        this.filmYear = filmYear;
+    }
+
+    public int getFilmLanguageId() {
+        return filmLanguageId;
+    }
+
+    public void setFilmLanguageId(int filmLanguageId) {
+        this.filmLanguageId = filmLanguageId;
+    }
+
+    public Long getFilmOriginalLanguajeId() {
+        return filmOriginalLanguajeId;
+    }
+
+    public void setFilmOriginalLanguajeId(Long filmOriginalLanguajeId) {
+        this.filmOriginalLanguajeId = filmOriginalLanguajeId;
+    }
+
+    public int getFilmRentalDuration() {
+        return filmRentalDuration;
+    }
+
+    public void setFilmRentalDuration(int filmRentalDuration) {
+        this.filmRentalDuration = filmRentalDuration;
+    }
+
+    public double getFilmRentalRate() {
+        return filmRentalRate;
+    }
+
+    public void setFilmRentalRate(double filmRentalRate) {
+        this.filmRentalRate = filmRentalRate;
+    }
+
+    public int getFilmLength() {
+        return filmLength;
+    }
+
+    public void setFilmLength(int filmLength) {
+        this.filmLength = filmLength;
+    }
+
+    public double getFilmReplacementCost() {
+        return filmReplacementCost;
+    }
+
+    public void setFilmReplacementCost(double filmReplacementCost) {
+        this.filmReplacementCost = filmReplacementCost;
+    }
+
+    public String getFilmRating() {
+        return filmRating;
+    }
+
+    public void setFilmRating(String filmRating) {
+        this.filmRating = filmRating;
+    }
+
+    public String getFilmFeatures() {
+        return filmFeatures;
+    }
+
+    public void setFilmFeatures(String filmFeatures) {
+        this.filmFeatures = filmFeatures;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
 
 }
--- src/main/java/cl/citiaps/spring/backend/rest/FilmService.java
+++ src/main/java/cl/citiaps/spring/backend/rest/FilmService.java
@@ -1,4 +1,21 @@
 package cl.citiaps.spring.backend.rest;
 
+import cl.citiaps.spring.backend.entities.Film;
+import cl.citiaps.spring.backend.repository.FilmRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/films")
 public class FilmService {
+
+    @Autowired
+    private FilmRepository filmRepository;
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public Iterable<Film> getAllFilms(){ return filmRepository.findAll(); }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Film findOne(@PathVariable("id") Integer id) { return filmRepository.findOne(id); }
 }
