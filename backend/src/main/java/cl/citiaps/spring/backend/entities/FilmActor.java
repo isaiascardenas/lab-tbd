0c41458158c02b4acebd5c260f5c61746e5f664e
Unknown <eduardo.bustamante@usach.cl>
2018-11-05 15:57:01 -0300

    Front y Back separados

---
+++ backend/src/main/java/cl/citiaps/spring/backend/entities/FilmActor.java
@@ -0,0 +1,9 @@
+package cl.citiaps.spring.backend.entities;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
