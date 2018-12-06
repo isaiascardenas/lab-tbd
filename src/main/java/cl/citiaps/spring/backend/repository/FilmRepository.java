06de1adbfca933e77f27e7215e59cd1960edbb4c
Isaías Cárdenas <isaias.cardenas@aphix.cl>
2018-12-06 11:43:54 -0300

    Limpia repositorio para actualización de proyecto

--- src/main/java/cl/citiaps/spring/backend/repository/FilmRepository.java
+++
@@ -1,7 +0,0 @@
-package cl.citiaps.spring.backend.repository;
-
-import cl.citiaps.spring.backend.entities.Film;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface FilmRepository extends PagingAndSortingRepository<Film, Integer> {
-}
