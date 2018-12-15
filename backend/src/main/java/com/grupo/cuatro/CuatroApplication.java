2051eedf1ebcc17628cf8ce8f48a8a5dc3c23e1a
jpm343 <juan.martinez.a@usach.cl>
2018-12-15 19:18:24 -0300

    planificacion mysql seeder

--- backend/src/main/java/com/grupo/cuatro/CuatroApplication.java
+++ backend/src/main/java/com/grupo/cuatro/CuatroApplication.java
@@ -1,12 +1,8 @@
 package com.grupo.cuatro;
 
-import com.grupo.cuatro.services.MysqlSeeder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -17,14 +13,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class CuatroApplication {
 
 	public static void main(String[] args) {
-
-
-            // this make null pointer exception...
-            // MysqlSeeder seeder = new MysqlSeeder();
-            // seeder.seed();
         Elastic e = new Elastic();
         SpringApplication.run(CuatroApplication.class, args);
-        System.out.println("count: " + e.getCantidad("tennis"));
 	}
 
 	@Bean
