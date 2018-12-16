cff559f704fc03e2a23ca6acc409051f8d9204b0
Unknown <jorge.plaza@usach.cl>
2018-12-15 21:23:55 -0300

    Fecha aun incompleto

--- backend/src/main/java/com/grupo/cuatro/Elastic.java
+++ backend/src/main/java/com/grupo/cuatro/Elastic.java
@@ -31,7 +31,8 @@ import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-
+import org.elasticsearch.search.DocValueFormat;
+import org.joda.time.DateTime;
 
 
 import javax.print.Doc;
@@ -74,7 +75,7 @@ public class Elastic {
                 doc = new Document();
                 doc.add(new StringField("id", cur.get("_id").toString(), Field.Store.YES));
                 doc.add(new TextField("text", cur.get("text").toString(), Field.Store.YES));
-                doc.add(new StringField("timeStamp", cur.get("createdAt").toString(),Field.Store.YES));
+                doc.add(new TextField("timeStamp", cur.get("createdAt").toString(),Field.Store.YES));
                 if(cur.get("location") != null){
                     doc.add(new TextField("location", cur.get("location").toString(), Field.Store.YES));
                 }
@@ -157,7 +158,9 @@ public class Elastic {
 
             return total;
         }
+
         public ArrayList<Integer> getCantidadFecha(String mes){
+
             ArrayList<Integer> resultados=new ArrayList<>();
             try {
                 IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get("indice/")));
@@ -165,7 +168,7 @@ public class Elastic {
 
                 Analyzer analyzer = new StandardAnalyzer();
                 QueryParser parser = new QueryParser("timeStamp", analyzer);
-                
+
                 for(int i=1;i<32;i++){
                     Query query = parser.parse(mes+" "+i);
                     TopDocs result = searcher.search(query, 25000);
